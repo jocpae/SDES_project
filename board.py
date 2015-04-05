2a8a3efd70aefe3033dedd24b3c647b0a1bfe80e
Pradeep Yadav <pradeepyadav@iitb.ac.in>
2015-04-06 03:17:10 +0530

    Upto make matrix to check working, drawing of the disks fixed

--- board.py
+++ board.py
@@ -33,7 +33,7 @@ class button():
 	
 	def is_clicked(self, m_pos):
 		if m_pos[0] > self.cx-self.l/2 and m_pos[0] < self.cx+self.l/2 and m_pos[1]>self.cy-self.h/2 and m_pos[1]<self.cy+self.h/2:
-			print "clicked"
+#			print "clicked"
 			flag = 1
 		else:
 			flag = 0
@@ -55,21 +55,29 @@ def default_canvas():
 	reset_button.draw()
 	if disks!=[]:
 		for d in disks:
-			cd_x = (d[0]+0.5)*w_height/num_rows
-			cd_y = (d[1]+0.5)* w_length/num_col
-			pygame.draw.circle(screen, BLACK, [cd_x, cd_y],15)
-
-		
-#		
-#def make_matrix_to_check(disks):
-#	
-#	
-#	
-#	
-#def check_winner(m):
-#	
+			cd_x = int((d[0]+0.5)* w_height/num_rows)
+			cd_y = int((d[1]+0.5)* w_length/num_col)
+			if disks.index(d)%2 ==0:
+				pygame.draw.circle(screen, BLACK, [cd_x, cd_y],15)
+			else:
+				pygame.draw.circle(screen, BLUE, [cd_x, cd_y],15)
+			
+def make_matrix_to_check(disks):
+	global num_col
+	to_check = np.zeros([num_col-1, num_col])
+	for d in disks:
+		if disks.index(d)%2 == 0:
+			to_check[d[1]][d[0]] = 1
+		else:
+			to_check[d[1]][d[0]] = 0
+	return to_check
 	
 	
+	
+def check_winner(m):
+	
+	
+	return 0
 	
 	
 	
@@ -85,11 +93,11 @@ GREY =  	(205,201, 201)
 PEACH =  	(255, 218, 185)
 pi = mat.pi
 
-num_col = 12
-num_rows = 10
-
+num_col = 10
+num_rows = num_col
+global num_col
 w_length = 400
-w_height = 400
+w_height = w_length
 size = [w_length, w_height]
 done = False
 screen = pygame.display.set_mode(size)
@@ -114,31 +122,32 @@ while not done:
 			mouse_pos = pygame.mouse.get_pos()
 			flag = reset_button.is_clicked(pygame.mouse.get_pos() )
 			if flag ==0:
-				print "use have not clicked on the reset button clicked at",mouse_pos 
+#				print "use have not clicked on the reset button clicked at",mouse_pos 
 				if mouse_pos[1]>w_height-0.5*w_height/num_rows:
 					# clicked on the empy space
-					print "clicked on empty space"
+#					print "clicked on empty space"
 					pass
 				else:
 					# Clicked on the road, get the indexes of the square and make the impression
 					i = mouse_pos[0]*num_col/w_length
-					j = mouse_pos[1]*num_rows/w_height+1
+					j = mouse_pos[1]*num_rows/w_height
 					if [i, j] in disks:
-						print i , j
+#						print i , j
 						pass
 					else:
-						print i , j
+#						print i , j
 						disks.append([i, j])
 					
 #				 reset the game
-			print disks
+#			print disks
 			# check if any of the box is clicked
 			
 			
-			print("User pressed a mouse button")
-			print 
-#	m = make_matrix_to_check(disks)
-#	winner = check_winner(m)
+#			print("User pressed a mouse button")
+
+	to_check = make_matrix_to_check(disks)
+	print to_check
+	winner = check_winner(to_check)	
 	default_canvas()
 	pygame.display.flip()
 	#print discrete_pts
